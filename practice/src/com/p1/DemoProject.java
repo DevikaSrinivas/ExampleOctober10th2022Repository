5081c4b33e610dde5276543dd08eb80b0068dea2
DevikaSrinivas <devikagowdalp996@gmail.com>
2023-01-02 17:54:36 +0530

    Display Numbers

---
+++ practice/src/com/p1/DemoProject.java
@@ -0,0 +1,20 @@
+package com.p1;
+
+public class DemoProject {
+
+	public static void main(String[] args)
+	{
+		int k=1;
+		while(k<=20)
+		{
+			if(k%2==0)
+			{
+				System.out.println(k);
+			
+			}
+			k++;
+		}
+
+	}
+
+}
---
+++ practice/src/com/sgtesting/constructorchaining/OddNumbers.java
@@ -0,0 +1,21 @@
+package com.sgtesting.constructorchaining;
+
+public class OddNumbers {
+
+	public static void main(String[] args) 
+	{
+		int sum=0;
+		for(int k=1;k<=50;k++)
+		{
+			if(k%2==1)
+			{
+				sum++;
+			}
+			
+			
+			}
+		System.out.println(sum);
+
+	}
+
+}
